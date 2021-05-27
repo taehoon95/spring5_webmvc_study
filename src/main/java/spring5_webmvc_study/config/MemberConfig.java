c745dc90c421404c254e2d9d5f85ecaa14173e65
taehoon95 <leeth950304@gmail.com>
2021-05-27 09:40:01 +0900

    예외처리

--- src/main/java/spring5_webmvc_study/config/MemberConfig.java
+++ src/main/java/spring5_webmvc_study/config/MemberConfig.java
@@ -1,5 +1,6 @@
 package spring5_webmvc_study.config;
 
+
 import org.apache.tomcat.jdbc.pool.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
--- src/main/java/spring5_webmvc_study/controller/MemberNotFoundException.java
+++ src/main/java/spring5_webmvc_study/controller/MemberNotFoundException.java
@@ -9,6 +9,4 @@ public class MemberNotFoundException extends RuntimeException{
 	public MemberNotFoundException(String message) {
 		super(message);
 	}
-
-	
 }
